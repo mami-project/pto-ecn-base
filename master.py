f04366113f9442be46e59ae1e56bb20300419917
Piet De Vaere <piet@devae.re>
2016-11-19 18:34:48 +0100

    added some debug prints

--- master.py
+++ master.py
@@ -53,24 +53,34 @@ def main():
     # analyze one upload per run
     upload_ids = [upload_ids[0]] if len(upload_ids) > 0 else []
 
-    print("running it with: ", max_action_id, upload_ids)
+    print("--> running it with: ", max_action_id, upload_ids)
     ac.set_result_info_direct(max_action_id, upload_ids)
 
     files = ac.spark_uploads_direct()
     # the RDD's returned by spark_uploads_direct() contain
     # the following KV pair: (filename, (metadata, file_content))
 
+    print("--> filenames before filter = {}".format(filenames))
+
     # This should be removed at some point
     files = files.filter(
         lambda x: x[1][0]['meta']['msmntCampaign'] == 'modern-times')
+    filenames = files.map(lambda x: x[0]).collect()
     files = files.filter(lambda x: '2lQnF34XCk' not in x[0])
 
+    print("--> filenames after filter = {}".format(filenames))
+
     flows = files.flatMap(unpack_files_to_flows)
+
+    print("number of flows = {}".format(flows.count()))
+
     observations = flows.map(flow_to_observation)
+
+    print("number of observations = {}".format(observations.count()))
 
     all_observations = observations.collect()
     all_observations_size_MiB = sys.getsizeof(all_observations) // 1024^2
-    print("Size of all_observations: {} MiB".format(all_observations_size_MiB))
+    print("--> Size of all_observations: {} MiB".format(all_observations_size_MiB))
 
     for observation in all_observations:
         ac.temporary_coll.insert_one(observation)
