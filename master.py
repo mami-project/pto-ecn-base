e15c7a51725a29faf7d92ae64cd88535df79d149
Piet De Vaere <piet@devae.re>
2016-11-20 11:44:29 +0100

    No more filtering

--- master.py
+++ master.py
@@ -55,37 +55,25 @@ def main():
     # analyze one upload per run
     upload_ids = [upload_ids[0]] if len(upload_ids) > 0 else []
 
-    print("--> running it with: ", max_action_id, upload_ids)
+    print("--> running with max action id: {}".format(max_action_id))
+    print("--> running with upload ids: {}".format(upload_ids))
     ac.set_result_info_direct(max_action_id, upload_ids)
 
-    files = ac.spark_uploads_direct()
-    print("--> number of files = {}".format(files.count()))
     # the RDD's returned by spark_uploads_direct() contain
     # the following KV pair: (filename, (metadata, file_content))
+    files = ac.spark_uploads_direct()
+    print("--> number of files: {}".format(files.count()))
 
     filenames = files.map(lambda x: x[0]).collect()
-    print("--> filenames before filter = {}".format(filenames))
-
-    # This should be removed at some point
-    files = files.filter(
-        lambda x: x[1][0]['meta']['msmntCampaign'] == 'modern-times')
-    files = files.filter(lambda x: '2lQnF34XCk' not in x[0])
-
-    filenames = files.map(lambda x: x[0]).collect()
-    print("--> filenames after filter = {}".format(filenames))
+    print("--> filenames: {}".format(filenames))
 
     flows = files.flatMap(unpack_files_to_flows)
-
-    print("--> number of flows = {}".format(flows.count()))
+    print("--> number of flows: {}".format(flows.count()))
 
     observations = flows.map(flow_to_observation)
-
-    print("--> number of observations = {}".format(observations.count()))
+    print("--> snumber of observations: {}".format(observations.count()))
 
     all_observations = observations.collect()
-    all_observations_size_MiB = sys.getsizeof(all_observations) // 1024^2
-    print("--> Size of all_observations: {} MiB".format(all_observations_size_MiB))
-
     for observation in all_observations:
         ac.temporary_coll.insert_one(observation)
 
